f0f34b1fba86e56cb2fa72dbecf153c020b692f7
Mo'men Ashraf Muhamed <79394414+Moamen189@users.noreply.github.com>
2023-01-01 17:23:39 +0200

    Time: 154 ms (68.34%), Space: 43.2 MB (6.89%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int[] RunningSum(int[] nums) {
+        int sum =0;
+        for(int i = 0; i < nums.Length; i++ ){
+            nums[i] += sum;
+            sum = nums[i];
+        }
+        return nums;
+        
+    }
+}
