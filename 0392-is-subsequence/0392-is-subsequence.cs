157af8eb49d8027ed1e982c14045118d7f90a5cc
Mo'men Ashraf Muhamed <79394414+Moamen189@users.noreply.github.com>
2023-01-02 19:28:40 +0200

    Time: 109 ms (48.77%), Space: 36.2 MB (96.66%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public bool IsSubsequence(string s, string t) {
+        int indexS = 0;
+        for (int indexT =0; indexS < s.Length && indexT <  t.Length;  indexT++){
+            if(s[indexS] == t[indexT])
+                indexS ++; 
+        }
+        
+        return indexS == s.Length;
+        
+    }
+}
