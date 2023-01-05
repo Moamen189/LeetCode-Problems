9d634b0bd5ce5c18716d870ce1dd7f01dc2fd5ba
Mo'men Ashraf Muhamed <79394414+Moamen189@users.noreply.github.com>
2023-01-05 16:42:12 +0200

    Time: 119 ms (15.81%), Space: 35 MB (75.06%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int LongestPalindrome(string s) {
+        
+        if (s == null || s.Length == null)
+            return 0;
+        
+        int len = 0;
+         HashSet<char> set = new HashSet<char>();
+        
+        foreach (char c in s){
+            
+            if (set.Contains(c)){
+                
+                len += 2;
+                set.Remove(c);
+            }else
+                set.Add(c);
+            
+        }
+        
+        return set.Count > 0 ? len +1  : len;
+        
+
+        
+    }
+}
