345677cfad05b0640d0a5cd86ceca7144d31a08d
Mo'men Ashraf Muhamed <79394414+Moamen189@users.noreply.github.com>
2023-01-08 01:45:51 +0200

    Time: 1028 ms (50.33%), Space: 0B (100.00%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,4 @@
+/* Write your T-SQL query statement below */
+SELECT Today.id FROM Weather Today, Weather Yesterday
+WHERE DATEDIFF(DAY, Today.recordDate, Yesterday.recordDate) = -1
+AND Today.temperature > Yesterday.temperature
