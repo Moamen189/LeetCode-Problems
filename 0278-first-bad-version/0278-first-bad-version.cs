b17cc91574e87e49117e835615238b99100627ee
Mo'men Ashraf Muhamed <79394414+Moamen189@users.noreply.github.com>
2023-01-07 19:02:14 +0200

    Time: 21 ms (94.74%), Space: 26.5 MB (25.93%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.cs
@@ -0,0 +1,11 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      bool IsBadVersion(int version); */
+
+public class Solution : VersionControl {
+    public int FirstBadVersion(int last, int first = 0) {
+        if (first == last) return first;
+        var middle = first + (last - first) / 2;
+        return IsBadVersion(middle) ? FirstBadVersion(middle, first) : FirstBadVersion(last, middle + 1);
+	}
+    
+}
