a8c950fc0ef9a8b07610665e06c2b73b97c62a93
Mo'men Ashraf Muhamed <79394414+Moamen189@users.noreply.github.com>
2023-01-09 21:43:14 +0200

    Time: 123 ms (84.51%), Space: 48.7 MB (41.45%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cs
+++ 0200-number-of-islands/0200-number-of-islands.cs
@@ -1,30 +1,33 @@
 public class Solution {
     public int NumIslands(char[][] grid) {
-         if (grid == null || grid.Length == 0)
+        if(grid == null || grid.Length ==0)
             return 0;
-
-        var counter = 0;
-        var yMax = grid.Length;
-        var xMax = grid[0].Length;
-
-        for (var y = 0; y < yMax; y++)
-        for (var x = 0; x < xMax; x++)
-            if (grid[y][x] == '1') {
-                TraversIsland(x, y);
-                counter++;
+        
+        var count =0;
+        var yMax= grid.Length;
+        var xMax= grid[0].Length;
+        
+        for(var x=0; x <xMax; x++)
+        for(var y=0; y <yMax; y++)
+            if(grid[y][x] =='1'){
+                
+                Island(x,y);
+                count++;
             }
-
-        return counter;
-
-        void TraversIsland(int x, int y) {
-            if (x < 0 || x >= xMax || y < 0 || y >= yMax || grid[y][x] != '1')
+        return count;
+        
+        void Island(int x,int y){
+             if (x < 0 || x >= xMax || y < 0 || y >= yMax || grid[y][x] != '1')
                 return;
 
             grid[y][x] = '0';
-            TraversIsland(x, y + 1);
-            TraversIsland(x, y - 1);
-            TraversIsland(x + 1, y);
-            TraversIsland(x - 1, y);
+            Island(x, y + 1);
+            Island(x, y - 1);
+            Island(x + 1, y);
+            Island(x - 1, y);
         }
+
+
+        
     }
 }
