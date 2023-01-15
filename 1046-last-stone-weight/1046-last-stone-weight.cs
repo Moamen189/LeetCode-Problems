b42b60ccd0c37605fa6e16f9e8dd63d7c5559ce2
Mo'men Ashraf Muhamed <79394414+Moamen189@users.noreply.github.com>
2023-01-15 23:34:41 +0200

    Time: 89 ms (74.58%), Space: 37.8 MB (48.60%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cs
@@ -0,0 +1,23 @@
+public class Solution {
+  public int LastStoneWeight(int[] stones) {
+    return SmashStones(stones,stones.Length -1);
+    
+}
+private int SmashStones(int[] stones, int end)
+{
+    if(end < 0)
+        return 0;
+    if(end == 0)
+        return stones[end];
+    Array.Sort(stones);
+    if(stones[end] == stones[end-1])
+    {
+        return SmashStones(stones,end-2);
+    }
+    else
+    {
+        stones[end-1] = Math.Abs(stones[end] - stones[end-1]);
+        return SmashStones(stones,end-1);
+    }
+}
+}
