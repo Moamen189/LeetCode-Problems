cb2ece7d6c9930af14fd491022bb0a385126ed57
Mo'men Ashraf Muhamed <79394414+Moamen189@users.noreply.github.com>
2023-01-17 21:49:34 +0200

    Time: 5179 ms (5.01%), Space: 0B (100.00%) - LeetHub

---
+++ 1393-capital-gain-loss/1393-capital-gain-loss.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT stock_name, ((SELECT sum(price) FROM Stocks s1 WHERE operation='Sell' AND s1.stock_name=s.stock_name)-(SELECT sum(price) FROM Stocks s2 WHERE operation='Buy' AND s2.stock_name=s.stock_name)) capital_gain_loss FROM Stocks s GROUP BY stock_name;
