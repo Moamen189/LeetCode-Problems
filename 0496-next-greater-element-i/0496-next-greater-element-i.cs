fca7d3158fdfb0cdb8fca6dbc2e45b9c5ea8c2dc
Mo'men Ashraf Muhamed <79394414+Moamen189@users.noreply.github.com>
2023-01-24 02:04:56 +0200

    Time: 236 ms (20.57%), Space: 42 MB (89.61%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int[] NextGreaterElement(int[] nums1, int[] nums2) {
+        	var dict = new Dictionary<int, int>();
+	var stack = new Stack<int>();
+
+	foreach (var n in nums2) {
+		while (stack.Any() && stack.Peek() < n)
+			dict[stack.Pop()] = n;
+		stack.Push(n);
+	}
+
+	return nums1.Select(x => dict.GetValueOrDefault(x, -1)).ToArray();
+    }
+}
