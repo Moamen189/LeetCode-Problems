369df1ec2f227bb3bf31eae4c8eba87be9074696
Mo'men Ashraf Muhamed <79394414+Moamen189@users.noreply.github.com>
2023-01-25 19:15:32 +0200

    Time: 267 ms (34.60%), Space: 47.3 MB (72.40%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public void MoveZeroes(int[] nums) {
+        	var n = nums.Length;
+	if (n < 2) return;
+
+	for (var i = n - 2; i >= 0; i--)
+	{
+		if (nums[i] != 0) continue;
+
+		for (var j = i; j < n - 1; j++)
+		{
+			if (nums[j + 1] == 0) break;
+
+			var tmp = nums[j];
+			nums[j] = nums[j + 1];
+			nums[j + 1] = tmp;
+		}
+	}
+    }
+}
