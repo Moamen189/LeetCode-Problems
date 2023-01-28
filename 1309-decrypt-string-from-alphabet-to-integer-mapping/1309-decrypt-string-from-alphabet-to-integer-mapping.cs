8e0b8d489238600d11c91b7d336025464e51b35c
Mo'men Ashraf Muhamed <79394414+Moamen189@users.noreply.github.com>
2023-01-28 19:16:28 +0200

    Time: 73 ms (94.69%), Space: 37.5 MB (27.43%) - LeetHub

---
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public string FreqAlphabets(string s) {
+        Stack<string> chars = new Stack<string>();
+	foreach(var c in s)
+	{
+		if(c == '#')
+		{
+			string pop1 = chars.Pop(), pop2 = chars.Pop();
+			chars.Push(pop2+pop1); //reverse the order so 10# => "10", not "01".
+		}
+		else
+		{
+			chars.Push(c.ToString());
+		}
+	}
+
+	return string.Join("", chars.Reverse().Select(x => (char)(int.Parse(x) + 'a' - 1)));
+    }
+}
