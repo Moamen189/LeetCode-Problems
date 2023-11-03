fcf05140e4fc7f96fcb242b0dd731c754c900e7a
Mo'men Ashraf Mohamed <79394414+Moamen189@users.noreply.github.com>
2023-11-03 14:45:14 +0200

    Time: 74 ms (89.37%), Space: 39.5 MB (30.62%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cs
+++ 0035-search-insert-position/0035-search-insert-position.cs
@@ -1,6 +1,11 @@
 public class Solution {
     public int SearchInsert(int[] nums, int target) {
-        int i = nums.ToList().BinarySearch(target);
-        return i >= 0 ? i: ~i;
+  for(int i = 0; i < nums.Length; i++){
+        if(nums[i] >= target){
+            return i;
+        }
     }
+    
+    return nums.Length;
+}
 }
