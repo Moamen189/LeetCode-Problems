ddc90ab1f560ce2aa484b853326d92ccf20ca641
Mo'men Ashraf Mohamed <79394414+Moamen189@users.noreply.github.com>
2023-11-04 15:43:52 +0200

    Time: 131 ms (35.21%), Space: 42.4 MB (78.71%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[] PlusOne(int[] digits) {
+        int n = digits.Length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        return result;
+    }
+    }
