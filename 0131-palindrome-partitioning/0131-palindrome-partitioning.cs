d3e0ce33f351a67c18b147fe91528394f057414e
Mo'men Ashraf Mohamed <79394414+Moamen189@users.noreply.github.com>
2023-11-17 16:18:29 +0200

    Time: 585 ms (14.09%), Space: 137.7 MB (89.09%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cs
@@ -0,0 +1,41 @@
+public class Solution {
+
+    List<IList<string>> res = new List<IList<string>>();
+
+    public IList<IList<string>> Partition(string s)
+    {
+        if (s == null || s.Length == 0) return null;
+
+        helper(new List<string>(), s, 0);
+
+        return res;
+    }
+
+    private void helper(List<string> list, string s, int index) 
+    {
+
+        if(index == s.Length)
+        {
+            res.Add(new List<string>(list));
+            return;
+        }
+
+        var cur = "";
+        for (var i = index; i < s.Length; i++) 
+        {
+            //cur = s.Substring(index, i- index + 1);
+            cur = cur + s[i];
+            if (isPalindromic(cur))
+            {
+                list.Add(cur);
+                helper(list, s, i + 1);
+                list.RemoveAt(list.Count - 1);  
+            }
+        }
+    }
+
+    public bool isPalindromic(string s) 
+    {
+        return s.SequenceEqual(s.Reverse());  
+    }
+}
