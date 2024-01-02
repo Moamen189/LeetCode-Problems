b526e8dea5e3245a2e602ce77b6873ed65294ebc
Mo'men Ashraf Mohamed <79394414+Moamen189@users.noreply.github.com>
2024-01-02 11:25:43 +0200

    Time: 238 ms (5.19%), Space: 48.5 MB (11.61%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cs
@@ -0,0 +1,51 @@
+public class Solution {
+    public int[] SearchRange(int[] nums, int target) {
+         int[] result = { -1, -1 };
+
+      
+        int left = 0, right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+            {
+                result[0] = mid;
+                right = mid - 1;
+            }
+            else if (nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        left = 0;
+        right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+            {
+                result[1] = mid;
+                left = mid + 1; 
+            }
+            else if (nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+}
