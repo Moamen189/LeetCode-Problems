88e5803de78dd0846483be358c401be2cc3af795
Mo'men Ashraf Mohamed <79394414+Moamen189@users.noreply.github.com>
2024-01-02 11:14:42 +0200

    Time: 109 ms (23.97%), Space: 44.2 MB (59.11%) - LeetHub

---
+++ 0018-4sum/0018-4sum.js
@@ -0,0 +1,50 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+function fourSum(nums, target) {
+    nums.sort((a, b) => a - b); // Sort the array
+
+    const result = [];
+
+    for (let i = 0; i < nums.length - 3; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+
+        for (let j = i + 1; j < nums.length - 2; j++) {
+            if (j > i + 1 && nums[j] === nums[j - 1]) {
+                continue;
+            }
+
+            let left = j + 1;
+            let right = nums.length - 1;
+
+            while (left < right) {
+                const sum = nums[i] + nums[j] + nums[left] + nums[right];
+
+                if (sum === target) {
+                    result.push([nums[i], nums[j], nums[left], nums[right]]);
+
+                    while (left < right && nums[left] === nums[left + 1]) {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] === nums[right - 1]) {
+                        right--;
+                    }
+
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+    }
+
+    return result;
+}
