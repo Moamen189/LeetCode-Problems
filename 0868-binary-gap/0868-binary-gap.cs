cb0e02db892f5ca1ac9680527b53b383ef98230e
Mo'men Ashraf Mohamed <79394414+Moamen189@users.noreply.github.com>
2024-01-26 23:41:32 +0200

    Time: 21 ms (60.00%), Space: 26.9 MB (55.56%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.cs
+++ 0868-binary-gap/0868-binary-gap.cs
@@ -1,12 +1,12 @@
 public class Solution {
     public int BinaryGap(int N) {
-        int i1 = 0;  // current
-        int i2 = 0;  // last 1
+        int i1 = 0;  
+        int i2 = 0;  
         int max = 0;
         while (N > 0)
         {
-            int temp = N % 2; // temp = N & 1 ;
-            N = N / 2; // N = N >> 1 ;
+            int temp = N % 2; 
+            N = N / 2; 
             i1++;
 
             if (temp == 1)
