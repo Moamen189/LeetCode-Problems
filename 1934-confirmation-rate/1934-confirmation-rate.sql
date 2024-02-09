45aea6182cc2a280772efb05eec83f635df58c36
Mo'men Ashraf Mohamed <79394414+Moamen189@users.noreply.github.com>
2024-02-09 15:05:22 +0200

    Time: 1239 ms (36.49%), Space: 0B (100.00%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+select
+s.user_id,
+round(avg(case when c.action = 'confirmed' then 1.00 else 0 end),2) as confirmation_rate
+from signups s
+left join confirmations c
+on s.user_id = c.user_id
+group by s.user_id
