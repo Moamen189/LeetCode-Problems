4bdf35e33d9daa153ba5db7af7e4519a472a8fd4
Mo'men Ashraf Mohamed <79394414+Moamen189@users.noreply.github.com>
2024-04-09 19:29:37 +0200

    Time: 63 ms (28.96%), Space: 40.7 MB (37.75%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public string GcdOfStrings(string str1, string str2) {
+        if(str1.Length<str2.Length)
+            return GcdOfStrings(str2,str1);
+        if(str2.Equals(str1))
+            return str1;
+        if(str1.StartsWith(str2))
+            return GcdOfStrings(str1.Substring(str2.Length),str2);
+
+        return "";
+    }
+}
