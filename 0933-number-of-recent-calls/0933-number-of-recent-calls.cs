c58319707dc6fd8d24829052ca29a1c23397b9f1
Mo'men Ashraf Mohamed <79394414+Moamen189@users.noreply.github.com>
2024-05-07 16:57:27 +0300

    Time: 264 ms (82.53%), Space: 100.5 MB (64.26%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.cs
@@ -0,0 +1,22 @@
+public class RecentCounter 
+{
+    private Queue<int> _queue = new();
+    
+    public int Ping(int t) 
+    {
+        _queue.Enqueue(t);
+
+        while(_queue.Count > 0 && _queue.Peek() < (t - 3000)) 
+        {
+            _queue.Dequeue();
+        }
+
+        return _queue.Count;
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.Ping(t);
+ */
