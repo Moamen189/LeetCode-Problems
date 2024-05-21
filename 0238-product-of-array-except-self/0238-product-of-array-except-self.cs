d21a1f0fad2f2a0caead82fc81b25ced99602b35
Mo'men Ashraf Mohamed <79394414+Moamen189@users.noreply.github.com>
2024-05-21 12:51:21 +0300

    Time: 181 ms (45.86%), Space: 65.7 MB (71.65%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
@@ -2,7 +2,7 @@ public class Solution {
 public int[] ProductExceptSelf(int[] nums)
     {
         int all = 1;
-        int zc = 0; //Zero Count
+        int zc = 0; 
         for (int i = 0; i < nums.Length; i++)
         {
             if (nums[i] == 0)
