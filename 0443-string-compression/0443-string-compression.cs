ff670c5d968944ce8f443fa37312846abf10e184
Mo'men Ashraf Mohamed <79394414+Moamen189@users.noreply.github.com>
2024-05-30 22:29:40 +0300

    Time: 138 ms (14.68%), Space: 63.4 MB (74.76%) - LeetHub

--- 0443-string-compression/0443-string-compression.cs
+++ 0443-string-compression/0443-string-compression.cs
@@ -1,52 +1,32 @@
   public class Solution
     {
-        public int Compress(char[] chars)
-        {
-            if (chars.Length <= 1)
-            {
-                return chars.Length;
-            }
+public int Compress(char[] chars) {
+    int n = chars.Length;
+    int writeIndex = 0;  // Index to write the next character in the chars array
+    int i = 0;
 
-            int currItem = 0;
-            char currentChar = chars[currItem];
-            int nextItem = 1;
-            int counter = 1;
+    while (i < n) {
+        char currentChar = chars[i];
+        int count = 0;
 
-            while (true)
-            {
-                if (nextItem > chars.Length)
-                {
-                    break;
-                }
-
-                if (nextItem == chars.Length || chars[nextItem] != currentChar)
-                {
-                    var prevChar = currentChar;
-                    currentChar = chars[Math.Min(nextItem, chars.Length - 1)];
-
-                    if (counter == 1)
-                    {
-                        chars[currItem++] = prevChar;
-                    }
-                    else
-                    {
-                        chars[currItem++] = prevChar;
-                        string counterStr = counter.ToString();
-                        foreach (var cstr in counterStr)
-                        {
-                            chars[currItem++] = cstr;
-                        }
-                    }
-
-                    counter = 1;
-                    nextItem++;
-                    continue;
-                }
-
-                counter++;
-                nextItem++;
-            }
-
-            return currItem;
+        // Count the number of occurrences of the current character
+        while (i < n && chars[i] == currentChar) {
+            i++;
+            count++;
         }
+
+        // Write the current character to the array
+        chars[writeIndex++] = currentChar;
+
+        // Write the count if it's more than 1
+        if (count > 1) {
+            foreach (char c in count.ToString()) {
+                chars[writeIndex++] = c;
+            }
+        }
+    }
+
+    // The writeIndex now points to the length of the compressed array
+    return writeIndex;
+}
     }
