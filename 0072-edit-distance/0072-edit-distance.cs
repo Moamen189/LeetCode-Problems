9fde64f838b5ce7590cfeca8686bd28df0fc93bd
Mo'men Ashraf Mohamed <79394414+Moamen189@users.noreply.github.com>
2024-06-01 20:13:40 +0300

    Time: 54 ms (56.00%), Space: 45.3 MB (24.27%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cs
+++ 0072-edit-distance/0072-edit-distance.cs
@@ -1,23 +1,18 @@
 public class Solution {
-    public int MinDistance(string text1, string text2)
+    public int MinDistance(string word1, string word2) => LevenshteinDistance(word1.ToArray(), word2.ToArray());        
+    public int LevenshteinDistance(char[] s1, char[] s2)
     {
-        int t1Length = text1.Length, t2Length = text2.Length;
-        var dp = new int[t1Length + 1, t2Length + 1];
-        foreach (var i in Enumerable.Range(0, t1Length + 1))
-        {
-            foreach (var j in Enumerable.Range(0, t2Length + 1))
-            {
-                if (i == 0 && j == 0) dp[i, j] = 0;
-                else if (i == 0) dp[i, j] = dp[i, j - 1] + 1;
-                else if (j == 0) dp[i, j] = dp[i - 1, j] + 1;
-                else if (text1[i - 1] == text2[j - 1]) dp[i, j] = dp[i - 1, j - 1];
-                else
-                {
-                    var minInherited = (new int[] { dp[i - 1, j - 1], dp[i - 1, j], dp[i, j - 1] }.Min());
-                    dp[i, j] = 1+ minInherited;
-                }
-            }
-        }
-        return dp[t1Length, t2Length];
+        int s1p = s1.Length, s2p = s2.Length;
+        int[,] num = new int[s1p + 1, s2p + 1];
+        for (int i = 0; i <= s1p; i++)
+            num[i,0] = i;
+        for (int i = 0; i <= s2p; i++)
+            num[0,i] = i;
+        for (int i = 1; i <= s1p; i++)
+            for (int j = 1; j <= s2p; j++)
+                num[i,j] = Math.Min(Math.Min(num[i - 1,j] + 1,
+                        num[i,j - 1] + 1), num[i - 1,j - 1]
+                        + (s1[i - 1] == s2[j - 1] ? 0 : 1));
+        return num[s1p,s2p];
     }
 }
