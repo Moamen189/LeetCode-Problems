13a71ca7fe8e75ad6a485e216507e19d80038304
Mo'men Ashraf Mohamed <79394414+Moamen189@users.noreply.github.com>
2024-06-02 20:20:21 +0300

    Time: 220 ms (71.77%), Space: 129.2 MB (77.81%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public void ReverseString(char[] s) {
+        int i = 0, j = s.Length-1;
+        while(i<j)
+        {
+            var temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
